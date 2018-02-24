84031704477af8ad930850939395a37733ab4eb3
Vinod Panicker <panickervinod@users.noreply.github.com>
2018-02-24 12:49:54 +0530

    Updating oah-remove git add *

--- src/bash/oah-help.sh
+++ src/bash/oah-help.sh
@@ -8,39 +8,20 @@ Install with ove:
 Usage:  oah install [option(-v,-d,-s)] {imagename}
  Options
  -v vagrant // will be used for testing and creation of cluster
-
--s standalone machine / will make use of localhost as inventory
-
+ -s standalone machine // will make use of localhost as inventory
+ -d docker // will make use of localhost as inventory
   Example : To Install Drupal8  on a Windows host with vagrant and Virtualbox:
- oah install -v oah-drupal8-vm
+  oah install -v oah-drupal8-vm
 
-
-List the VMs available:
-oah list
-
- Show the status of the  environmant through vagrant:
-oah status
-
-Show the  environments installed on the vm:
-oah show
-
-Show the  current environment installed on the vm:
-oah show  current
-
-Reset- Resets the environment back:
-oah reset - run from the guest
-
-Remove- Remove all the installed environment and make it as a base machine:
-oah remove - run from guest
-
-Destroy the guest:
-oah destroy [machine id]}
-
-Provision the vm - Run this if the ove install options fails in between:
-oah provision
-
-Halt a guest:
-oah halt
+	oah list //List the VMs available:
+	oah status // Show the status of the  environmant through vagrant:
+	oah show // Show the  environments installed on the vm:
+	oah show current //Show the  current environment installed on the vm:
+	oah reset //- run from the guest, Resets the environment back:
+	oah remove // - run from guest,Remove- Remove all the installed environment and make it as a base machine:
+	oah destroy [machine id]} //Destroy the guest:
+	oah provision //Provision the vm - Run this if the ove install options fails in between:
+	oah halt //Halt a guest:
 
 EOF
 }
--- src/bash/oah-remove.sh
+++ src/bash/oah-remove.sh
@@ -6,14 +6,19 @@ function __oah_remove {
 # ansible-playbook -i ${OAH_PROVISIONING_DIR}/inventory ${OAH_PROVISIONING_DIR}/ove-remove.yml
 
 CURRENT_ENV=`find $OAH_DIR/data/env -mindepth 1 -maxdepth 1  -exec basename {} \;`
+echo "Current env is $CURRENT_ENV"
+
 if [[ -n "$CURRENT_ENV" ]]
 then
 if [[ "$CURRENT_ENV" = "oah-vm" ]]
+ echo "Skipping as current env is $CURRENT_ENV"
 then
  echo "Current env is already the default oah-vm ,Nothing to remove . Skipping..."
 else
  #TODO Check why roles are installed first.
+ echo "Install required roles!!"
  ansible-galaxy install -r $OAH_DIR/data/env/$CURRENT_ENV/provisioning/oah-requirements.yml -p $OAH_DIR/data/roles
+ echo "Executing playbook : $OAH_DIR/data/env/$CURRENT_ENV/provisioning/oah-remove.yml  "
  ansible-playbook $OAH_DIR/data/env/$CURRENT_ENV/provisioning/oah-remove.yml -K
  rm -rf $OAH_DIR/data/env/$CURRENT_ENV
 fi
